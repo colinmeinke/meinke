dfe7f8861f1843c83b2276cf32f8fbc687e82442
Colin Meinke <hello@colinmeinke.com>
2016-12-22 09:03:41 +0000

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default () => (
+  <div>
+    <h1>
+      Graham Meinke
+    </h1>
+
+    <h2>
+      Service Development & Business Transformation
+    </h2>
+
+    <p>
+      Supporting the development of new products and services
+      and major change projects.
+    </p>
+  </div>
+)
